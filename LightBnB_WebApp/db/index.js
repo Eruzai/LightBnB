a0b1a876c40a025ee2023475ff6e551071574577
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-19 09:35:18 -0700

    feat: centralize query function, pool, and export

---
+++ LightBnB_WebApp/db/index.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+ 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+const query = (text, params, callback) => {
+  return pool.query(text, params, callback)
+};
+
+module.exports = {
+  pool,
+  query
+};
